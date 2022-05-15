c2d033a52c2b60f1bfeb015528bafffb7834b568
Andrew Freeman <andrew.p.freeman@outlook.com>
2022-05-15 13:16:06 -0400

    End of 3.15'

---
+++ assets/js/game.js
@@ -0,0 +1,27 @@
+// This Creates a function Named "fight"
+
+var playerName= window.prompt("What is your Robot's Name");
+
+//What is this??
+
+console.log(playerName);
+
+console.log("This logs a strig, good for leaving yourself a message");
+// This will do math and log 20 
+
+console.log(10 +10);
+
+//What is this
+
+console.log("Our robot's name is" + playerName);
+
+//Note the lack of Quotation marks around playername
+
+window.alert(playerName);
+
+function fight () {
+    window.alert("The Fight Has Begun");
+}
+
+//fight();
+
